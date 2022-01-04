7f39bde7a7c5838011a8a1617305ea961a965b5c
alwihaque <61383019+alwihaque@users.noreply.github.com>
2022-01-04 23:22:19 +0600

    Added Default API Key Headers to Axios

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ import axios from 'axios';
 
 export class OpenSea {
     constructor(API_KEY = "") {
-        this.API_KEY = API_KEY
+        this.API_KEY = API_KEY;
+        axios.defaults.headers.common['X-API-KEY'] = API_KEY;
     }
 
     async singleAsset({asset_contract_address, token_id, account_address}) {
         const baseURL = `https://api.opensea.io/api/v1/asset/${asset_contract_address}/${token_id}/`
         try {
             return await axios.get(baseURL, {
-                headers: {
-                    'X-API-KEY': this.API_KEY
-                },
                 params: {
                     account_address
                 }
@@ -24,11 +22,7 @@ export class OpenSea {
     async singleContract ({asset_contract_address}) {
         const baseUrl = `https://api.opensea.io/api/v1/asset_contract/${asset_contract_address}`
         try {
-            return await axios.get(baseUrl, {
-                headers: {
-                    'X-API-KEY': this.API_KEY
-                }
-            });
+            return await axios.get(baseUrl);
         }
         catch (e) {
             console.log(e.message);
@@ -36,6 +30,8 @@ export class OpenSea {
     }
 
 }
+
+
 
 
 
